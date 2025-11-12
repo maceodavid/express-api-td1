a9e9ea7bef185a11403360e177d3a1bc16e79092
Maceo <maceod23@gmail.com>
2025-11-12 17:55:22 +0100

    users table

--- src/db/schema.js
+++ src/db/schema.js
@@ -6,5 +6,14 @@ export const questionstable = sqliteTable("questions", {
     questionText: text("question_text", { length: 300 }).notNull(),
     answer: text({ length: 300 }).notNull(),
     difficulty: text({ enum: ["easy", "medium", "difficult"] }).notNull().default("easy"),
+    createdAt: integer("created_at", { mode: "timestamp" }).notNull().$defaultFn(() => new Date()),
+    author: text().references(() => usersTable.id, { onDelete: "cascade" }).notNull()
+});
+
+export const usersTable = sqliteTable("users", {
+    id: text().primaryKey().$defaultFn(randomUUID),
+    email: text().notNull().unique(),
+    username: text({ length: 30 }).notNull(),
+    password: text({ length: 255 }).notNull(),
     createdAt: integer("created_at", { mode: "timestamp" }).notNull().$defaultFn(() => new Date())
 });
--- src/db/seed.js
+++ src/db/seed.js
@@ -1,29 +1,50 @@
 import { db } from "./database.js";
-import { questionstable } from "./schema.js";
+import { questionstable, usersTable } from "./schema.js";
+import bcrypt from "bcrypt";
 
 async function seed() {
     try {
         console.log("Seeding database...");
 
         await db.delete(questionstable);
+        await db.delete(usersTable);
+
+		const hashedPassword1 = await bcrypt.hash("password", 12);
+		const hashedPassword2 = await bcrypt.hash("1234", 12);
+
+        const seedUsers = [
+			{
+				email: "clement.catel@unicaen.fr",
+				username: "clement",
+				password: hashedPassword1,
+			},
+			{
+				email: "michel@unicaen.fr",
+				username: "michel",
+				password: hashedPassword2,
+			},
+		]
+
+        const insertedusers = await db.insert(usersTable).values(seedUsers).returning();
 
         const seedQuestions = [
 			{
 				questionText: 'Quelle est la capitale de la France?',
 				answer: 'Paris',
 				difficulty: 'easy',
-				// createdBy: insertedusers[0].id,
+				author: insertedusers[0].id,
 			},
 			{
 				questionText: 'Quel est le plus grand océan du monde?',
 				answer: "L'océan Pacifique",
 				difficulty: 'medium',
-				// createdBy: insertedusers[1].id,
+				author: insertedusers[1].id,
 			},
 			{
 				questionText: 'Qui a écrit "Les Misérables"?',
 				answer: 'Victor Hugo',
 				difficulty: 'difficult',
+				author: insertedusers[1].id,
 			},
 		]
 
